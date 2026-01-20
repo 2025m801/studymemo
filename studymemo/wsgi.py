88d7d314267aa7ef8783325e3f5dd38b05a4b8a7
“2025m801 <2025m801@hokuriku-u.ac.jp>
2026-01-20 18:22:39 +0800

    Fix DJANGO_SETTINGS_MODULE to studymemo

--- studymemo/wsgi.py
+++ studymemo/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devProject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "studymemo.settings")
+
+
+
 
 application = get_wsgi_application()
